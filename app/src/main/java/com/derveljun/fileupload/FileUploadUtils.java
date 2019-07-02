145e3a4105023c7863cd55b101332bbbb10ec9a4
derveljun <derveljun@tistory.com>
2019-07-02 17:47:15 +0900

    init

---
+++ app/src/main/java/com/derveljun/fileupload/FileUploadUtils.java
@@ -0,0 +1,44 @@
+package com.derveljun.fileupload;
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class FileUploadUtils {
+
+    public static void goSend(File file){
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("files", file.getName(), RequestBody.create(MultipartBody.FORM, file))
+                .build();
+
+        Request request = new Request.Builder()
+                .url("http://192.168.43.221:18080/upload")
+                .post(requestBody)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("TEST : ", response.body().string());
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/derveljun/fileupload/MainActivity.java
@@ -0,0 +1,97 @@
+package com.derveljun.fileupload;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgVwSelected;
+    Button btnImageSend, btnImageSelection;
+    File tempSelectFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnImageSend = findViewById(R.id.btnImageSend);
+        btnImageSend.setEnabled(false);
+        btnImageSend.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                FileUploadUtils.goSend(tempSelectFile);
+            }
+        });
+
+
+        btnImageSelection = findViewById(R.id.btnImageSelection);
+        btnImageSelection.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                // Intent를 통해 이미지를 선택
+                Intent intent = new Intent();
+                // intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+
+
+            }
+        });
+
+        imgVwSelected = findViewById(R.id.imgVwSelected);
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode != 1 || resultCode != RESULT_OK) {
+            return;
+        }
+
+        Uri dataUri = data.getData();
+        imgVwSelected.setImageURI(dataUri);
+
+        try {
+            // ImageView 에 이미지 출력
+            InputStream in = getContentResolver().openInputStream(dataUri);
+            Bitmap image = BitmapFactory.decodeStream(in);
+            imgVwSelected.setImageBitmap(image);
+            in.close();
+
+            // 선택한 이미지 임시 저장
+            String date = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss").format(new Date());
+            tempSelectFile = new File(Environment.getExternalStorageDirectory()+"/Pictures/Test/", "temp_" + date + ".jpeg");
+            OutputStream out = new FileOutputStream(tempSelectFile);
+            image.compress(Bitmap.CompressFormat.JPEG, 100, out);
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+
+        btnImageSend.setEnabled(true);
+
+    }
+}
